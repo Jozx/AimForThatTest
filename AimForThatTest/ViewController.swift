bbe87f1154dc78415b868b0dafe87822831cc3bc
Jose Saracho <jsaracho1@gmail.com>
2018-08-03 20:54:36 -0400

    Se agregan usabilidad de las distintas etiquetas (suman rondas, suman puntos) Se generaron numerosas aleatorios para las distintas rondas Se compara entre el numero pedido y el que acierta el usuario a través del slider

--- AimForThatTest/ViewController.swift
+++ AimForThatTest/ViewController.swift
@@ -9,10 +9,24 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var currentValue : Int = 0
+    var targetValue  : Int = 0
+    var score        : Int = 0
+    var round        : Int = 0
+    
+    @IBOutlet weak var slider: UISlider!
+    @IBOutlet weak var targetLabel: UILabel!
+    @IBOutlet weak var scoreLabel: UILabel!
+    @IBOutlet weak var roundLabel: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        newRound()
+        updateLabels()
     }
 
     override func didReceiveMemoryWarning() {
@@ -21,19 +35,50 @@ class ViewController: UIViewController {
     }
 
     @IBAction func showAlert() {
-        let alert = UIAlertController(title: "Hola Mundo", message: "Este es el juego Aim For That", preferredStyle: .alert)
+        
+        let diffence : Int = abs(self.currentValue - self.targetValue)
+        
+        let points : Int = (diffence > 0) ? 100 - diffence : 1000 //ternario.. si es diferencia es mayor a 0 .. 100 - diff... sino  1000 puntos
+        
+        self.score += points
+        
+        let message = """
+            Has marcado \(points) puntos!!!
+        """
+        
+        
+        
+        let alert = UIAlertController(title: "Hola Mundo", message: message, preferredStyle: .alert)
+
         
         let action = UIAlertAction(title: "Cerrar", style: .destructive, handler: nil)
         
         alert.addAction(action)
         
         present(alert, animated: true, completion: nil)
+        
+        newRound()
+        updateLabels()
     }
     
     @IBAction func sliderMove(_ sender: UISlider) {
-        print("el valor del slider es \(sender.value)")
+        
+        self.currentValue = Int(sender.value)
+        
+        print("el valor del slider es \(self.currentValue)")
     }
     
+    func newRound() {
+        self.targetValue = 1 + Int(arc4random_uniform(100)) //genera los valores aleatorios entre 1 y 100
+        self.currentValue = 50
+        self.slider.value = Float(self.currentValue)
+        self.round += 1
+    }
     
+    func updateLabels () {
+        self.targetLabel.text = "\(self.targetValue)"
+        self.scoreLabel.text = "\(self.score)"
+        self.roundLabel.text = "\(self.round)"
+    }
 }
 
